d54223831c32ef41edafcc85e02db09cfc982875
Michael Buluma <buluma@users.noreply.github.com>
2022-02-07 19:30:25 +0300

    local init

---
+++ terraform/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
